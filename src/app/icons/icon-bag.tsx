c28c832a93b5ef632ce5a7a0e56657d1a44368f1
vmnog <vmnogueira.ti@gmail.com>
2024-05-24 21:14:35 -0300

    fix(icons): use camelCase attributes in svg

--- src/app/icons/icon-bag.tsx
+++ src/app/icons/icon-bag.tsx
@@ -1,5 +1,5 @@
 export function IconBagSVG() {
   return (
-    <svg className="w-8 min-w-8" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.14096 8.00024C6.71925 8.00024 5.49276 8.99817 5.20365 10.3902L3.99972 16.1868C3.48396 18.6701 5.37989 21.0002 7.91614 21.0002H16.0839C18.6201 21.0002 20.516 18.6701 20.0003 16.1868L18.7964 10.3902C18.5073 8.99818 17.2808 8.00024 15.8591 8.00024H8.14096Z" stroke-width="1.5" stroke-linecap="round" strokeLinejoin="round" stroke="#ea1d2c"></path><path d="M15 6V6C15 4.34315 13.6569 3 12 3V3C10.3431 3 9 4.34315 9 6V6" stroke-width="1.5" stroke-linecap="round" strokeLinejoin="round" stroke="#ea1d2c"></path></svg>
+    <svg className="w-8 min-w-8" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.14096 8.00024C6.71925 8.00024 5.49276 8.99817 5.20365 10.3902L3.99972 16.1868C3.48396 18.6701 5.37989 21.0002 7.91614 21.0002H16.0839C18.6201 21.0002 20.516 18.6701 20.0003 16.1868L18.7964 10.3902C18.5073 8.99818 17.2808 8.00024 15.8591 8.00024H8.14096Z" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" stroke="#ea1d2c"></path><path d="M15 6V6C15 4.34315 13.6569 3 12 3V3C10.3431 3 9 4.34315 9 6V6" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" stroke="#ea1d2c"></path></svg>
   )
 }
