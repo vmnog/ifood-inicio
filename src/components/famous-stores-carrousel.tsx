e5d99ab70b384fb0f3fd2d9f540a00b6d7edfaa3
vmnog <vmnogueira.ti@gmail.com>
2024-05-24 23:56:39 -0300

    fix(famous-stores-carrousel): add key to first element

--- src/components/famous-stores-carrousel.tsx
+++ src/components/famous-stores-carrousel.tsx
@@ -4,12 +4,12 @@ export function FamousStoresCarrousel() {
   return (
     <div className="grid grid-cols-2 gap-14 2xl:grid-cols-3">
       {Array.from({ length: 12 }).map((_, index) => (
-        <div className="flex items-center gap-4">
-          <Skeleton key={index} className="w-20 h-20 rounded-sm" />
+        <div key={index} className="flex items-center gap-4">
+          <Skeleton className="w-20 h-20 rounded-sm" />
           <div className="flex flex-col justify-stretch gap-2 h-full">
-            <Skeleton key={index} className="w-64 h-6 rounded-sm" />
-            <Skeleton key={index} className="w-60 h-4 rounded-sm" />
-            <Skeleton key={index} className="mt-auto w-32 h-3 rounded-sm" />
+            <Skeleton className="w-64 h-6 rounded-sm" />
+            <Skeleton className="w-60 h-4 rounded-sm" />
+            <Skeleton className="mt-auto w-32 h-3 rounded-sm" />
           </div>
         </div>
       ))}
